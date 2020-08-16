3a14f88814ab33ec0449612cf16d375e64ae13d3
Trevor Allen <tallen94@users.noreply.github.com>
2020-08-16 00:09:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="python_modules", # Replace with your own username
+    name="pythonmodules", # Replace with your own username
     version="0.0.1",
     packages=setuptools.find_packages(),
 )
